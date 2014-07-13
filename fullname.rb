308051f6839042a748325ddf636c1feb519356a9
swatkns785 <steven.watkins78@gmail.com>
2014-07-13 15:18:52 -0400

    Create fullname.rb
    Code for asking first name, middle name, last name and combining them in a sentence.

---
+++ fullname.rb
@@ -0,0 +1,7 @@
+puts 'What is your first name?'
+var1 = gets.chomp
+puts 'What is your middle name?'
+var2 = gets.chomp
+puts 'What is your last name?'
+var3 = gets.chomp
+puts 'Hello! ' + var1 + ' ' + var2 + ' ' + var3 + ' is a great name!'
