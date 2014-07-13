61aa77f7bf49f59d87bc6bd2fa45ff1153f8b3ae
swatkns785 <steven.watkins78@gmail.com>
2014-07-13 16:11:18 -0400

    Update favoritenumber.rb

--- favoritenumber.rb
+++ favoritenumber.rb
@@ -2,3 +2,8 @@ puts 'What is your favorite number?'
 var1 = gets.chomp
 var2 = var1.to_i + 1
 puts var2.to_s + ' is a bigger and better favorite number.'
+
+puts 'What is your favorite number?'
+var1 = gets.chomp
+var2 = var1.to_i + 1
+puts var2.to_s + ' is a bigger and better favorite number than ' + var1.to_s + '.'
