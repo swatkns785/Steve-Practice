14b7a52cb6676a0d8d8ecc092c3f9aead5dc7bc2
swatkns785 <steven.watkins78@gmail.com>
2014-07-13 16:44:00 -0400

    Create fullnamechar.rb
    Calculates the number of characters in a given full name.

---
+++ fullnamechar.rb
@@ -0,0 +1,8 @@
+puts 'What is your first name?'
+var1 = gets.chomp
+puts 'What is your middle name?'
+var2 = gets.chomp
+puts 'What is your last name?'
+var3 = gets.chomp
+char = var1.length.to_i + var2.length.to_i + var3.length.to_i
+puts 'Did you know that there are ' + char.to_s + ' characters in your name?!'
