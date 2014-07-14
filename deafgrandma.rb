de00b85fb831efe759f0202dcd3c50e4cbd7ca02
swatkns785 <steven.watkins78@gmail.com>
2014-07-14 14:48:06 -0400

    Update deafgrandma.rb
    had trouble with keeping the conversation going with grandma and defining a range (i have an old macbook and maybe i'm running an old version of ruby)

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,8 +1,14 @@
 puts 'Ask me anything and I\'ll tell you the truth!'
 response = gets.chomp
-if response != response.upcase
-  puts 'HUH?! SPEAK UP SONNY!'
 
-elsif response == response.upcase
-  puts 'NOT SINCE 1938!'
+while response != response.upcase
+  puts 'HUH?! SPEAK UP SONNY!'
+  response = gets.chomp
+end
+
+if response == response.upcase
+  puts('NOT SINCE ' + rand(1951).to_s + '!')
+
+  else response == 'BYE BYE BYE'
+  puts 'Goodbye, sweetheart!'
 end
